7e9b00ea10673fd355c0c6024003d47ed98f30ef
Suchintita07 <suchintita2001@gmail.com>
2026-04-17 00:49:35 +0530

    Update student_analysis.py

--- student_analysis.py
+++ student_analysis.py
@@ -15,3 +15,19 @@ print("\n--- Analysis Result ---")
 print("Average Marks:", average)
 print("Highest Marks:", highest)
 print("Lowest Marks:", lowest)
+
+print("\n--- Student Grades ---")
+
+for i in range(n):
+    if marks[i] >= 80:
+        grade = "A"
+    elif marks[i] >= 60:
+        grade = "B"
+    elif marks[i] >= 40:
+        grade = "C"
+    else:
+        grade = "Fail"
+
+    status = "Pass" if marks[i] >= 40 else "Fail"
+
+    print(f"Student {i+1}: Marks = {marks[i]}, Grade = {grade}, Status = {status}")
